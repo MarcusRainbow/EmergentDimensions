0e899c13960b9066115f78d59860454f4cbad348
Marcus Rainbow <40654783+MarcusRainbow@users.noreply.github.com>
2021-10-02 06:01:00 +0100

    Many more tests
    Add uncoords, which reverses the action of coords. This makes it easy to add new tests and validate the existing ones

--- coords.hs
+++ coords.hs
@@ -52,7 +52,7 @@ coord_constructor s p x =
         if n < m then let b = p VV.! n in
             (s!0^2 - s!n^2 + sum2 b - 2 * (sum_pair b x)) / (2 * V.last b)
         else
-            sqrt ((V.last s)^2 - sum2_diff x (VV.last p))
+            safe_sqrt ((V.last s)^2 - sum2_diff x (VV.last p))
 
 -- |Sum of the squares of the items in a vector
 sum2 :: Vector Float -> Float
@@ -67,6 +67,53 @@ sum_pair v1 v2 = V.sum $ V.zipWith (*) v1 v2
 sum2_diff :: Vector Float -> Vector Float -> Float
 -- todo is there a way of doing this without an intermediate vector?
 sum2_diff v1 v2 = sum2 $ V.zipWith (-) v1 v2
+
+-- |Reverse the action of coords. Given a set of coordinates, calculate
+-- |the distances between them.
+uncoords :: VV.Vector (Vector Float) -> VV.Vector (Vector Float)
+uncoords c = let n = VV.length c - 1 in
+    VV.generate n (uncoords_generator c)
+
+-- |Construct a set of distances from one of a set of coordinates
+uncoords_generator :: VV.Vector (Vector Float) -> Int -> Vector Float
+uncoords_generator c i =
+    generate (i + 1) (find_distance c (i + 1))
+
+-- |Find the distance between two points given the set of coordinates
+find_distance :: VV.Vector (Vector Float) -> Int -> Int -> Float
+find_distance c i j = calc_distance (c VV.! i) (c VV.! j)
+
+-- |Calculate the distance between two coordinates
+calc_distance :: Vector Float -> Vector Float -> Float
+calc_distance x y = safe_sqrt ((sum2_diff x y) + (sum2_rem x y))
+
+-- |Given two vectors of different lengths, sum the squares of the
+-- |tail elements in the longer vector
+sum2_rem :: Vector Float -> Vector Float -> Float
+sum2_rem x y =
+    let
+        lx = V.length x
+        ly = V.length y
+        d = lx - ly
+    in case compare d 0 of
+        LT -> sum2 (unsafeDrop lx y)
+        EQ -> 0.0
+        GT -> sum2 (unsafeDrop ly x)
+
+-- |Safer version of sqrt, which will not give NaN for slightly negative
+safe_sqrt :: Float -> Float
+safe_sqrt x = 
+    if x >= 0.0 || x < (-1e-6) then
+        sqrt x
+    else
+        0.0
+
+-- |Various tests of safe_sqrt
+test_safe_sqrt :: Float
+test_safe_sqrt = let
+    result = (safe_sqrt 4.0) + (safe_sqrt (-1e-7)) in
+    assert (result == 2.0)
+    result 
 
 -- |Test sum of squares
 test_sum2 :: Float
@@ -103,6 +150,32 @@ test_sum2_diff_mismatch :: Float
 test_sum2_diff_mismatch = let 
     result = sum2_diff (fromList [11.0, 22.0]) (fromList [10.0, 20.0, 30.0]) in
     assert (result == 5.0) result
+
+-- |Test sum2_rem where second vector is empty
+test_sum2_rem :: Float
+test_sum2_rem = let 
+    result = sum2_rem (fromList [3.0]) empty in
+    assert (result == 9.0) 
+    result
+
+-- |Test sum2_rem where first vector is longer
+test_sum2_rem_gt :: Float
+test_sum2_rem_gt = let 
+    result = sum2_rem (fromList [1.0, 2.0, 3.0, 4.0]) (fromList [10.0, 20.0]) in
+    assert (result == 25.0) result
+
+-- |Test sum2_rem where second vector is longer
+test_sum2_rem_lt :: Float
+test_sum2_rem_lt = let 
+    result = sum2_rem (fromList [1.0, 2.0]) (fromList [10.0, 20.0, 30.0, 40.0]) in
+    assert (result == 2500.0) result
+
+-- Test calc_distance for two vectors of different lengths
+test_calc_distance :: Float
+test_calc_distance = let
+    result = calc_distance (fromList [3.0]) (fromList []) in
+    -- assert (result == 3.0)
+    result
 
 -- |Test coord constructor for intermediate step
 test_coord_constructor :: Float
@@ -204,6 +277,7 @@ test_coords_simplex = let
         [1.0],
         [0.5, 0.8660254],
         [0.5,0.28867513,0.8164966]]))
+    assert (approx_equal_vv 1e-6 sample_dists (uncoords result))
     result
 
 -- |Overall test, given a collection of 5 points all one apart
@@ -221,6 +295,21 @@ test_coords_4d_simplex = let
         [0.5, 0.8660254],
         [0.5,0.28867513,0.8164966],
         [0.5,0.28867513,0.20412417,0.7905694]]))
+    assert (approx_equal_vv 1e-6 sample_dists (uncoords result))
+    result
+
+
+-- |Round trip test of a near-equilateral 4d simplex
+test_roundtrip_4d_simplex :: VV.Vector (Vector Float)
+test_roundtrip_4d_simplex = let
+    sample_dists = fromLists [
+        [1.01],
+        [1.02, 1.03],
+        [1.04, 1.05, 1.06],
+        [1.07, 1.08, 1.09, 1.0]]
+    c = coords sample_dists
+    result = uncoords c in
+    assert (approx_equal_vv 1e-6 sample_dists result)
     result
 
 -- |Test of a set of points that form a 3, 4, 5 triangle
@@ -234,6 +323,7 @@ test_coords_345 = let
         [],
         [3.0],
         [0.0, 4.0]]))
+    assert (approx_equal_vv 1e-6 sample_dists (uncoords result))
     result
 
 -- |Test of a set of points with one degenerate dimension
@@ -244,10 +334,43 @@ test_coords_degenerate_345 = let
         [4.0, 5.0],
         [5.0, 4.0, 3.0]]
     result = coords sample_dists in
+    assert (approx_equal_vv 1e-6 result (fromLists [
+        [],
+        [3.0],
+        [0.0, 4.0],
+        [3.0, 4.0, 0.0]]))
+    assert (approx_equal_vv 1e-6 sample_dists (uncoords result))
+    result
+
+-- |Test of a set of points with one degenerate dimension plus another point
+test_coords_degenerate_345_plus :: VV.Vector (Vector Float)
+test_coords_degenerate_345_plus = let
+    sample_dists = fromLists [
+        [3.0],
+        [4.0, 5.0],
+        [5.0, 4.0, 3.0], -- todo another set of distances
+        []]
+    result = coords sample_dists in
     -- assert (approx_equal_vv 1e-6 result (fromLists [
     --     [],
-    --     [1.0],
-    --     [0.5, 0.5 * sqrt 3]]))
+    --     [3.0],
+    --     [0.0, 4.0],
+    --     [3.0, 4.0, 0.0]]))
+    result
+
+-- |Test the reverse calculation of distances from coords
+test_uncoords :: VV.Vector (Vector Float)
+test_uncoords = let
+    sample_coords = fromLists [
+        [],
+        [3.0],
+        [0.0, 4.0],
+        [3.0, 4.0, 0.0]]
+    result = uncoords sample_coords in
+    assert (approx_equal_vv 1e-6 result (fromLists [
+        [3.0],
+        [4.0, 5.0],
+        [5.0, 4.0, 3.0]]))
     result
 
 -- |Approx absolute comparison of floats
@@ -280,12 +403,17 @@ main = do
     putStrLn "Running tests..."
     putStrLn $ "test_vector_from_list: " ++ (show test_vector_from_list)
     putStrLn $ "test_from_lists: " ++ (show test_from_lists)
+    putStrLn $ "test_safe_sqrt: " ++ (show test_safe_sqrt)
     putStrLn $ "test_sum2: " ++ (show test_sum2)
     putStrLn $ "test_sum_pair: " ++ (show test_sum_pair)
     putStrLn $ "test_sum_pair_mismatch: " ++ (show test_sum_pair_mismatch)
     putStrLn $ "test_sum_pair_mismatch2: " ++ (show test_sum_pair_mismatch2)
     putStrLn $ "test_sum2_diff: " ++ (show test_sum2_diff)
     putStrLn $ "test_sum2_diff_mismatch: " ++ (show test_sum2_diff_mismatch)
+    putStrLn $ "test_sum2_rem: " ++ (show test_sum2_rem)
+    putStrLn $ "test_sum2_rem_lt: " ++ (show test_sum2_rem_lt)
+    putStrLn $ "test_sum2_rem_gt: " ++ (show test_sum2_rem_gt)
+    putStrLn $ "test_calc_distance: " ++ (show test_calc_distance)
     putStrLn $ "test_coord_constructor: " ++ (show test_coord_constructor)
     putStrLn $ "test_coord_constructor_final: " ++ (show test_coord_constructor_final)
     putStrLn $ "test_coord_constructor_problem: " ++ (show test_coord_constructor_problem)
@@ -297,7 +425,9 @@ main = do
     putStrLn $ "test_calc_coord_second: " ++ (show test_calc_coord_second)
     putStrLn $ "test_coords_simplex: " ++ (show test_coords_simplex)
     putStrLn $ "test_coords_4d_simplex: " ++ (show test_coords_4d_simplex)
+    putStrLn $ "test_roundtrip_4d_simplex: " ++ (show test_roundtrip_4d_simplex)
     putStrLn $ "test_coords_345: " ++ (show test_coords_345)
     putStrLn $ "test_coords_degenerate_345: " ++ (show test_coords_degenerate_345)
+    putStrLn $ "test_uncoords: " ++ (show test_uncoords)
     putStrLn "...all tests passed"
     putStrLn ""
