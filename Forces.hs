9a1583dc046e369eb0db9360ed9e2ffed3a390cc
Marcus Rainbow <40654783+MarcusRainbow@users.noreply.github.com>
2021-10-21 21:29:26 +0100

    Fix zip-related bugs
    The problem with zip is that it truncates to the shortest vector. My guess is that it is not very efficient for vectors either, as it makes a temporary vector, which surely hits the heap. Fix both problems by creating new functions foldr2 and foldr3, which behave like foldr except that they take two/three argument vectors.

--- Forces.hs
+++ Forces.hs
@@ -1,3 +1,9 @@
+module Forces (
+    Force,
+    power_law,
+    forces
+) where
+
 import Data.Vector.Unboxed hiding (foldr1, foldr, force, sum, length, last, zipWith, map, (++))
 import qualified Data.Vector.Unboxed as V
 import qualified Data.Vector as VV
@@ -27,21 +33,22 @@ type Force = Double
 -- |assumes the same power law for every distance. Arguably, the cutoff when
 -- |counting dimensions depends on the distance scale, so we could try a power
 -- |law that has more dimensions for smaller distances.
-power_law :: VV.Vector (Vector C.Coord) -> (Distance -> Double)
+power_law :: C.Matrix C.Coord -> (Distance -> Double)
 power_law c = let count = count_coord_dimensions c in
     (\s -> 1.0 / (s^(count - 1))) -- count - 1, because we differentiate
 
 -- |Given a vector of coordinates, return a vector of forces applied to
--- |each distance between two points
-forces :: VV.Vector (Vector C.Distance) -> VV.Vector (Vector C.Coord) -> VV.Vector (Vector Force)
-forces c = forces_given_power_law (power_law c) c
+-- |each distance between two points. Returns a string in case of error
+-- |(though this is not currently used).
+forces :: C.Matrix C.Distance -> C.Matrix C.Coord -> Either String (C.Matrix Force)
+forces s c = Right (forces_given_power_law (power_law c) c s)
 
 -- |Given a vector of coordinates and a power law,
 -- |return a vector of forces applied to
 -- |each distance between two points
 forces_given_power_law :: (Distance -> Double) -> 
-    VV.Vector (Vector C.Coord) -> VV.Vector (Vector C.Distance) -> 
-    VV.Vector (Vector Force)
+    C.Matrix C.Coord -> C.Matrix C.Distance -> 
+    C.Matrix Force
 forces_given_power_law f c s = VV.generate (VV.length c) generator where
     generator i = V.generate (V.length (c VV.! i)) (generator' i)
     generator' i = force f c s i
@@ -49,7 +56,7 @@ forces_given_power_law f c s = VV.generate (VV.length c) generator where
 -- |Find the force between two points i and j in a vector of coordinates.
 -- |We need to add up the dot products of each vector ik with the vector ij
 force :: (Distance -> Double) -> 
-    VV.Vector (Vector C.Coord) -> VV.Vector (Vector C.Distance) ->
+    C.Matrix C.Coord -> C.Matrix C.Distance ->
     Int -> Int -> Force
 force f c s i j = foldr (add_both_components f c s i j) 0.0 [0..n] where
     n = (VV.length c) - 1
@@ -58,7 +65,7 @@ force f c s i j = foldr (add_both_components f c s i j) 0.0 [0..n] where
 -- |from point i to j. Adds the effect of the point k relative to i, and
 -- |relative to j.
 add_both_components :: (Distance -> Double) -> 
-    VV.Vector (Vector C.Coord) -> VV.Vector (Vector C.Distance) ->
+    C.Matrix C.Coord -> C.Matrix C.Distance ->
     Int -> Int -> Int -> Force -> Force
 add_both_components f c s i j k acc =
     -- trace ("add_both_components i=" ++ show i ++ " j=" ++ show j ++ " k=" ++ show k)
@@ -67,7 +74,7 @@ add_both_components f c s i j k acc =
 -- |Adds one vector component to the overall force in the direction of the distance
 -- |from point i to j. The component added is due to point k relative to point i
 add_component :: (Distance -> Double) -> 
-    VV.Vector (Vector C.Coord) -> VV.Vector (Vector C.Distance) ->
+    C.Matrix C.Coord -> C.Matrix C.Distance ->
     Int -> Int -> Int -> Force -> Force
 add_component f c s i j k acc = 
     if i == k then 
@@ -81,43 +88,26 @@ add_component f c s i j k acc =
             factor = f dist_ik
         in
             -- trace ("add_component i=" ++ show i ++ " j=" ++ show j ++ " k=" ++ show k ++ " dot=" ++ show dot)
-            -- trace ("    dist_ij=" ++ show dist_ij ++ " dist_ik=" ++ show dist_ik ++ " factor" ++ show factor)
+            -- trace ("    dist_ij=" ++ show dist_ij ++ " dist_ik=" ++ show dist_ik ++ " factor=" ++ show factor)
             acc + dot * factor / (dist_ij * dist_ik)
 
 -- |Calculate the dot product of vector ik and vector ij within coordinate set c_ij
-dot_product :: VV.Vector (Vector Double) -> Int -> Int -> Int -> Double
+dot_product :: Matrix Double -> Int -> Int -> Int -> Double
 dot_product c i j k = 
     let
         ci = c VV.! i
         cj = c VV.! j
         ck = c VV.! k
     in
-        V.sum $ izipWith (\l x y -> let z = (get_or 0.0 ci l) in (x - z) * (y - z)) cj ck
+        C.foldr3 (\x y z a -> a + (y - x) * (z - x)) 0.0 0.0 ci cj ck
 
 -- |Fetch the distance between two points i and j within a distance set s_ij
-distance :: VV.Vector (Vector C.Distance) -> Int -> Int -> Distance
+distance :: C.Matrix C.Distance -> Int -> Int -> Distance
 distance s i j =
     if i > j then
         (s VV.! (i-1)) ! j
     else
         (s VV.! (j-1)) ! i
-
--- |Gets a value from a vector or a default if off the end
-get_or :: Unbox a => a -> Vector a -> Int -> a
-get_or def c i = case c!?i of
-    Just x -> x
-    Nothing -> def
-
--- |Tests the get_or method, before and after the end of the vector
-test_get_or :: (Int, Int)
-test_get_or = 
-    let
-        v = V.fromList [1, 2]
-        a = get_or 0 v 1
-        b = get_or 0 v 2
-    in
-        assert (a == 2 && b == 0) 
-        (a, b)     
 
 -- |Tests the distance method, with the end points in either order
 test_distances :: (Distance, Distance, Distance)
@@ -132,17 +122,31 @@ test_distances =
         (s01, s13, s30)
 
 -- |Tests the dot product function
+-- test_dot_product :: Double
+-- test_dot_product =
+--     let
+--         c = fromLists [[], [1.0], [1.1, 2.1], [1.2, 2.2, 3.2]]
+--         (i, j, k) = (1, 2, 3)
+--         -- vector ij = (1.1 - 1.0, 2.1 - 0.0) = (0.1, 2.1) 
+--         -- vector ik = (1.2 - 1.0, 2.2 - 0.0, 3.2 - 0.0) = (0.2, 2.2, 3.2)
+--         -- ij.ik = 0.1 * 0.2 + 2.1 * 2.2 = 4.64
+--         result = dot_product c i j k
+--     in
+--         assert (C.approx_equal 1e-6 result 4.64)
+--         result
+
 test_dot_product :: Double
 test_dot_product =
     let
-        c = fromLists [[], [1.0], [1.1, 2.1], [1.2, 2.2, 3.2]]
-        (i, j, k) = (1, 2, 3)
-        -- vector ij = (1.1 - 1.0, 2.1 - 0.0) = (0.1, 2.1) 
-        -- vector ik = (1.2 - 1.0, 2.2 - 0.0, 3.2 - 0.0) = (0.2, 2.2, 3.2)
-        -- ij.ik = 0.1 * 0.2 + 2.1 * 2.2 = 4.64
+        c = fromLists [
+            [],
+            [1.0],
+            [0.5, 0.8660254]]
+        (i, j, k) = (2, 1, 1)
+        -- ij.ik = 0.5 * 0.5 + 0.866254 * 0.866254 = 1.0
         result = dot_product c i j k
     in
-        assert (C.approx_equal 1e-6 result 4.64)
+        assert (C.approx_equal 1e-6 result 1.0)
         result
 
 -- |Tests the force function, assuming a simple factor function
@@ -159,14 +163,14 @@ test_force =
             [1.0],
             [0.5, 0.8660254],
             [0.5,0.28867513,0.8164966]]
-        result = force (\_ -> 1.0) coords dists 1 3
+        result = force (\_ -> 1.0) coords dists 1 2
     in
-        assert (C.approx_equal 1e-6 result 1.75)
+        assert (C.approx_equal 1e-6 result 4.0)
         result
 
 -- |Tests the forces_given_power_law function, assuming a simple factor function
 -- |that always returns 1.0 operating on a simplex of points
-test_forces_given_power_law :: VV.Vector (Vector Force)
+test_forces_given_power_law :: C.Matrix Force
 test_forces_given_power_law =
     let
         dists = fromLists [
@@ -180,25 +184,37 @@ test_forces_given_power_law =
             [0.5,0.28867513,0.8164966]]
         expected = fromLists [
             [],
-            [2.0],
-            [2.0, 1.75],
-            [2.0, 1.75, 1.33333333333]]
+            [4.0],
+            [4.0, 4.0],
+            [4.0, 4.0, 4.0]]
         result = forces_given_power_law (\_ -> 1.0) coords dists 
     in
         assert (C.approx_equal_vv 1e-6 result expected)
         result
 
+-- |Tests the forces function on an equilateral simplex of dimension n
+test_forces_eq_simplex :: Either String (C.Matrix Force)
+test_forces_eq_simplex =
+    let
+        dists = fromLists [
+            [1.0],
+            [1.0, 1.0],
+            [1.0, 1.0, 1.0]]
+    in
+        test_forces dists
+
 -- |Tests the forces function on a random simplex of dimension n
-test_forces_simplex :: StatefulGen g m => g -> Int -> m (Either String (VV.Vector (Vector Force)))
+test_forces_simplex :: StatefulGen g m => g -> Int -> m (Either String (C.Matrix Force))
 test_forces_simplex rnd n = do
     simplex <- C.create_random_triangular_matrix rnd (0.99, 1.01) n
     return (test_forces simplex)
 
 -- |Tests the forces function on a given set of distances
-test_forces :: VV.Vector (Vector Distance) -> Either String (VV.Vector (Vector Force))
+test_forces :: VV.Vector (Vector Distance) -> Either String (C.Matrix Force)
 test_forces s = do
     c <- C.validated_coords s
-    return (forces c s)
+    f <- forces s c
+    return f
 
 main :: IO ()
 main = do
@@ -206,11 +222,11 @@ main = do
     putStrLn ""
     putStrLn "Running Forces tests..."
 
-    putStrLn $ "test_get_or: " ++ (show test_get_or)
     putStrLn $ "test_distances: " ++ (show test_distances)
     putStrLn $ "test_dot_product: " ++ (show test_dot_product)
     putStrLn $ "test_force: " ++ (show test_force)
     putStrLn $ "test_forces_given_power_law: " ++ (show test_forces_given_power_law)
+    putStrLn $ "test_forces_eq_simplex: " ++ (show test_forces_eq_simplex)
 
     -- One generator shared by all tests that need random numbers
     rnd <- createSystemRandom
